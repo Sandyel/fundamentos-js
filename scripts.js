b3862e4fa0d80591aedbe467acd7d5b383a4af20
Sandyel Gomes <sandyelferreia409@gmail.com>
2021-11-18 23:59:41 -0300

    Foi implementado as funções handleNumberInput() e handleMinMax() com o objetivo de tratar a entrada de dado no input

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
-const userNumberElement = document.querySelector("#userNumber"),
-  sendButton = document.querySelector("#sendButton"),
-  resetButton = document.querySelector("#resetButton"),
-  minNumberElement = document.querySelector("#minNumberElement"),
-  maxNumberElement = document.querySelector("#maxNumberElement"),
-  tipElement = document.querySelector("#tip"),
-  guessesRemainigElement = document.querySelector("#guessesRemainigElement");
+const userNumberElement = document.querySelector('#userNumber'),
+  sendButton = document.querySelector('#sendButton'),
+  resetButton = document.querySelector('#resetButton'),
+  minNumberElement = document.querySelector('#minNumberElement'),
+  maxNumberElement = document.querySelector('#maxNumberElement'),
+  tipElement = document.querySelector('#tip'),
+  guessesRemainigElement = document.querySelector('#guessesRemainigElement');
+
+  userNumberElement.addEventListener('input', handleNumberInput);
+
 
 const minNumber = 0,
   maxNumber = 10,
@@ -30,6 +33,18 @@ function start() {
 
 function generateNumber() {
   Math.floor(Math.random() * (maxNumber + 1 - minNumber)) + minNumber;
+}
+
+
+function  handleNumberInput(even){
+  let value = parseInt(event.target.value || userNumber || 0);
+  value = handleMinMax(minNumber, value, maxNumber);
+  userNumber = value;
+  event.target.value = value;
+}
+
+function handleMinMax(min, number, max){
+  return Math.min(Math.max(number, min), max);
 }
 
 start();
